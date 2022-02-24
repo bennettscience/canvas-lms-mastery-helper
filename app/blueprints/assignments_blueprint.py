5d0f6f41abda0c61ea858a12be11dfb95e1519d6
bennettscience <brian.bennett2@gmail.com>
2022-02-24 15:31:38 -0500

    remove unsupported HTTP method

--- app/blueprints/assignments_blueprint.py
+++ app/blueprints/assignments_blueprint.py
@@ -9,7 +9,7 @@ from controllers.assignments import (
 assignments_view = AssignmentListAPI.as_view("assignment_list_view")
 assignment_view = AssignmentAPI.as_view("assignment_view")
 
-assignments_bp.add_url_rule("/assignments", view_func=assignments_view, methods=['GET', 'POST'])
+assignments_bp.add_url_rule("/assignments", view_func=assignments_view, methods=['POST'])
 assignments_bp.add_url_rule("/assignments/push", view_func=assignments_view, methods=['PUT'])
 assignments_bp.add_url_rule("/assignments/<int:assignment_id>", view_func=assignment_view, methods=['GET'])
 assignments_bp.add_url_rule("/assignments/<int:assignment_id>/push", view_func=assignment_view, methods=['PUT'])
