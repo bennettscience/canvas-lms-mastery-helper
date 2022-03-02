a715595d70b7a9ed4bfd56dc048751dd9f2367ab
bennettscience <brian.bennett2@gmail.com>
2022-03-02 13:15:58 -0500

    fix decorator for class methods

--- app/util.py
+++ app/util.py
@@ -1,13 +1,18 @@
-from flask import abort, redirect
+from functools import wraps
+from flask import abort, g, redirect, request, url_for
 from flask_login import current_user
 
 
-def teacher_required(func):
-    def check_is_teacher(*args, **kwargs):
-        if current_user.is_anonymous:
-            return redirect('/')
-        else:
-            if current_user.user_type != "Teacher":
-                return abort(401, "You do not have permission to view this page.")
-            return func(*args, **kwargs)
-    return check_is_teacher()
+def restricted():
+    def _restricted(func):
+        @wraps(func)
+        def __restricted(*args, **kwargs):
+            if current_user.is_anonymous:
+                return redirect(url_for('home_bp.index', next=request.url))
+            else:
+                # Redirect students away from restricted endpoints
+                if current_user.usertype_id == 3:
+                    return abort(401, "You do not have permission to view this page.")
+                return func(*args, **kwargs)
+        return __restricted
+    return _restricted
